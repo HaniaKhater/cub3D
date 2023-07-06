cec330343bac6ed724cfc258fe5491a1c84b189f
Hania hesham KHATER <hkhater@paul-f3Br3s4.clusters.42paris.fr>
2023-07-07 01:12:10 +0200

    adjusted the main return values and norminette of cub3d.h after merge updates

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hkhater <hkhater@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 17:08:52 by hania             #+#    #+#             */
-/*   Updated: 2023/07/07 00:30:38 by hkhater          ###   ########.fr       */
+/*   Updated: 2023/07/07 00:35:21 by hkhater          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ int	main(int ac, char **av)
 		return (free_scene(input), ft_printf("Error\nIncorrect input\n"), 1);
 	print_scene(input);
 	if (game_init(&game, input) == 0)
-		return (0);
+		return (1);
 	if (t_mlx_is_valid(game.mlx) == 0)
-		return (0);
+		return (1);
 	mlx_hook(game.mlx->win_ptr, 2, 1L << 0, &key_down_hook, &game);
 	mlx_hook(game.mlx->win_ptr, 3, 1L << 1, &key_up_hook, &game);
 	mlx_hook(game.mlx->win_ptr, 17, 0, &close_window, game.mlx);
@@ -34,4 +34,5 @@ int	main(int ac, char **av)
 	mlx_loop(game.mlx->mlx_ptr);
 	free_scene(input);
 	game_free(game);
+	return (0);
 }
